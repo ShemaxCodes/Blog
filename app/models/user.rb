b91f8b52c6ceefd175bf095e0e4f563c71a0456b
jashema_panapa <jcpanapa@gmail.com>
2021-02-10 15:08:43 -0500

    modified:   app/models/post.rb 	modified:   app/models/user.rb 	new file:   db/migrate/01_create_users.rb 	new file:   db/migrate/02_create_posts.rb 	new file:   db/migrate/03_create_comments.rb 	new file:   db/schema.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base 
-
-
+    has_many :posts
+    has_secure_password
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
 
 
---
+++ db/migrate/02_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+    def change
+      create_table :posts do |t|
+        t.string :title
+        t.text :body
+        t.datetime :created_at, null: false
+        t.datetime :updated_at, null: :false 
+        
+      end
+   end
+end

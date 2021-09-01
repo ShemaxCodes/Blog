d968268a1ba8a4bd02d2aca3527111812cc069c9
Jashema Panapa <jcpanapa@gmail.com>
2021-09-01 16:54:29 -0400

    modified:   app/views/posts/index.html.erb 	modified:   app/views/posts/show.html.erb 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
  get "/posts", to: "posts#index"
   get "/posts/new", to: "posts#new"
   post "/posts", to: "posts#create"
-  get "posts/:id", to: "posts#show"
+  get "posts/:id", to: "posts#show", as: "post"
 
 
 

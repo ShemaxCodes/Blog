36a2fc1dc74b7d221d6485bbc33e340ea3b9781f
Jashema Panapa <jcpanapa@gmail.com>
2021-09-07 16:21:57 -0400

    new file:   app/controllers/authors/posts_controller.rb 	deleted:    app/controllers/posts_controller.rb 	modified:   app/models/post.rb 	modified:   config/routes.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base 
-has_many :authors
+belongs_to :author
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "posts#index"
-
-
-
- get "/posts", to: "posts#index"
+  scope module: 'authors' do 
+    
+  get "/posts", to: "posts#index"
   get "/posts/new", to: "posts#new"
   post "/posts", to: "posts#create"
   get "posts/:id", to: "posts#show", as: "post"
 
   delete '/posts/:id', to: 'posts#destroy', as: "delete"
-
+  end 
 
 end

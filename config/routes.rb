3c7b84195a3d4b2c5cd1b318b4e1f8ff5bb613c8
Jashema Panapa <jcpanapa@gmail.com>
2021-09-07 16:25:38 -0400

    modified:   app/controllers/home_controller.rb 	renamed:    app/views/posts/index.html.erb -> app/views/authors/posts/index.html.erb 	renamed:    app/views/posts/new.html.erb -> app/views/authors/posts/new.html.erb 	renamed:    app/views/posts/show.html.erb -> app/views/authors/posts/show.html.erb 	new file:   app/views/home/index.html.erb 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "posts#index"
+  root to: "home#index"
   scope module: 'authors' do 
-    
+
   get "/posts", to: "posts#index"
   get "/posts/new", to: "posts#new"
   post "/posts", to: "posts#create"
